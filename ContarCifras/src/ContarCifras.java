3a176ce0ff432f84c853c01e68fbcc3fcfebef57
Estudiante USC <estudiante@GTACAD.USC>
2022-09-30 16:35:40 -0500

    trabajoEnClase

--- ContarCifras/src/ContarCifras.java
+++ ContarCifras/src/ContarCifras.java
@@ -5,21 +5,26 @@ public class ContarCifras {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Definir y pedir variables
 		Scanner entrada = new Scanner(System.in);
-		
 		int c = 0;
 		System.out.println("DIgite el número");
 		int numero=entrada.nextInt();
-		int real=numero;
+		int numOriginal=numero;
+		
+		//Si el número tiene una sola cifra hacer que  contador (cifras) = 1
 		if(numero/10==0) {
 			c=1;
-		}else {
+		}
+		//sino hacer el proceso para contar las cifras
+		else {
 			while(numero!=0) {
 			numero=numero/10;
 			c++;
 			}
 		}
-System.out.println("Tiene "+c+" cifras");
+		//imprimir la cantidad de cifras
+System.out.println("El número "+numOriginal+" Tiene "+c+" cifras");
 		
 		
 
--- TrabajoClase2/src/TrabajoEnClaseDos.java
+++ TrabajoClase2/src/TrabajoEnClaseDos.java
@@ -6,14 +6,16 @@ public class TrabajoEnClaseDos {
 		// TODO Auto-generated method stub
 		
 		Scanner entrada = new Scanner(System.in);
-		
+		//definir y pedir variables
 		System.out.println("DIgite el año");
 		int year=entrada.nextInt();
-			
+		//detectar los años bisiestos y no bisiestos	
 		if ((year % 400 == 0)||(year % 4 == 0) && ((year % 100 != 0))) {
+			//imprimir por pantalla si es bisiesto
 			System.out.println("El año "+ year +" es bisiesto");
 		}
 		else {
+			//imprimir por pantalla si no es bisiesto
 			System.out.println("El año "+year+" no es bisiesto");
 		}
 	}	
